76cc93adff7d97994ebdd83bbda225888c82d603
martapanc <marta.panc@gmail.com>
2016-07-01 20:32:11 +0200

    adding java files

---
+++ FiscalCodeCalculator/FiscalCodeCalculator.java
@@ -0,0 +1,53 @@
+package FiscalCodeCalculator;
+
+import java.lang.reflect.Method;
+import java.lang.reflect.Constructor;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import javax.swing.JFrame;
+import methods.DataPanel;
+import methods.FCCalculations;
+import methods.Town;
+
+public class FiscalCodeCalculator {
+	
+	public static void main (String[] args) throws NoSuchMethodException, SecurityException {		
+		JFrame frame1 = new JFrame();
+	    frame1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        DataPanel dp = new DataPanel();
+        frame1.getContentPane().add(dp);
+        frame1.setLocation(200,100);
+        frame1.pack();
+        frame1.setVisible(true);	
+                
+        System.out.println("************************************************\n    Italian Fiscal Code Calculator project\n************************************************");
+        Class<FCCalculations> calc = FCCalculations.class;
+        System.out.println("Class name: " + calc.getName());
+        System.out.println("Method List:");
+        Method[] methods = calc.getDeclaredMethods();
+        for (Method m : methods)
+        	System.out.println(" - " + m.getName() + " (" + Arrays.toString(m.getParameterTypes()) + ")  \treturns: " + m.getReturnType());
+        Class<Town> town = Town.class;
+        System.out.println ("\n***********************************************\nClass name: " + town.getName());
+        @SuppressWarnings("rawtypes")
+		Constructor[] c = town.getConstructors();
+        System.out.println("Constructor: " + Arrays.toString(c));
+        System.out.println("Method List: ");
+        methods = town.getDeclaredMethods();
+        for (Method m : methods)
+        	System.out.println(" - " + m.getName() + " (" + Arrays.toString(m.getParameterTypes()) + ")\treturns: " + m.getReturnType());
+        List<String> ls = new ArrayList<String>();
+        ls.add(0, "element");
+        String s = ls.get(0);
+        System.out.println(s);
+
+	}
+	
+	static <S> void fromArrayToCollection(S[] a, Collection<S> c) {
+	    for (S o : a) {
+	        c.add(o); // Correct
+	    }
+	}
+}
---
+++ methods/Town.java
@@ -0,0 +1,32 @@
+package methods;
+
+public class Town {
+	
+	String townName, townCode;
+	
+	public Town (String townName, String townCode) {
+			this.townName = townName;
+			this.townCode = townCode;
+	}
+	
+	public String getTownName() {
+		return townName;
+	}
+	
+	public String getTownCode() {
+		return townCode;
+	}
+	
+	public void setTownName(String newName) {
+		townName = newName;
+	}
+	
+	public void setTownCode(String newCode) {
+		townCode = newCode;
+	}
+	
+	public String toString() {
+		return getTownName() + " " + getTownCode();
+	}
+
+}
